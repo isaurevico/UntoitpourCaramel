94ad89f188acd14e461abb2e8e39d6e6e185d733
Slyvred <remihage2003@gmail.com>
2023-03-24 09:44:55 +0100

    ajout code test

---
+++ test.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const options = {
+  method: "GET",
+  url: "https://seloger.p.rapidapi.com/properties/list",
+  params: {
+    zipCodes: "75",
+    pageIndex: "1",
+    pageSize: "50",
+    realtyTypes: "1",
+    transactionType: "1",
+    sortBy: "0",
+    includeNewConstructions: "true",
+  },
+  headers: {
+    "X-RapidAPI-Key": "1dfeb33feemshd1ffb63572a1339p1e89b4jsn287f125ec49c",
+    "X-RapidAPI-Host": "seloger.p.rapidapi.com",
+  },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
